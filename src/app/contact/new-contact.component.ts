acd9dedc2b6e5863a5172c971d77ec9c5bf1f002
Thuan T. DINH <dinhtienthuan@gmail.com>
2016-12-23 18:22:42 +0700

    Implementing: Forms using FormBuilder in Angular 2. I'm going to use formControlName instead of formControl.

--- src/app/contact/new-contact.component.ts
+++ src/app/contact/new-contact.component.ts
@@ -2,32 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import {ContactService} from "./contact.service";
 import {Contact} from "./contact";
 import {Router, ActivatedRoute, Params} from "@angular/router";
+import {FormGroup, FormBuilder, Validators, NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-new-contact',
   template: `
-    <form #contactForm (ngSubmit)="onSubmit()">
+    <form [formGroup]="contactForm" (ngSubmit)="onSubmit(contactForm)">
       <div>
         <label for="firstName">First Name:</label>
-        <input type="text" id="firstName" name="firstName" required [(ngModel)]="newContact.firstName" #firstName>
-        <span *ngIf="!firstName.checkValidity()">Not valid</span>
+        <input type="text" id="firstName" name="firstName" [formControl]="contactForm.controls['firstName']" formControlName="firstName" >
+        <span *ngIf="!contactForm.controls.firstName.valid">Not valid</span>
       </div>
       <div>
         <label for="lastName">Last Name:</label>
-        <input type="text" id="lastName" name="lastName" required [(ngModel)]="newContact.lastName" #lastName>
-        <span *ngIf="!lastName.checkValidity()">Not valid</span>
+        <input type="text" id="lastName" name="lastName" [formControl]="contactForm.controls['lastName']">
+        <span *ngIf="!contactForm.controls['lastName'].valid">Not valid</span>
       </div>
       <div>
         <label for="phone">Phone Number:</label>
-        <input type="text" id="phone" name="phone" required [(ngModel)]="newContact.phone" #phone>
-        <span *ngIf="!phone.checkValidity()">Not valid</span>
+        <input type="text" id="phone" name="phone" [formControl]="contactForm.controls['phone']">
+        <span *ngIf="!contactForm.controls['phone'].valid">Not valid</span>
       </div>
       <div>
         <label for="email">Email:</label>
-        <input type="text" id="email" name="email" required [(ngModel)]="newContact.email" #email>
-        <span *ngIf="!email.checkValidity()">Not valid</span>
+        <input type="text" id="email" name="email" [formControl]="contactForm.controls['email']">
+        <span *ngIf="!contactForm.controls['email'].valid">Not valid</span>
       </div>
-      <button type="submit" [disabled]="!contactForm.checkValidity()">Create Contact</button>
+      <button type="submit" [disabled]="!contactForm.valid">Create Contact</button>
     </form>
   `,
   styles: [`
@@ -46,23 +47,27 @@ import {Router, ActivatedRoute, Params} from "@angular/router";
   `]
 })
 export class NewContactComponent implements OnInit {
-  newContact: Contact;
+  contactForm: FormGroup;
 
-  constructor(private contactService: ContactService, private router: Router, private activatedRoute: ActivatedRoute) { }
+  constructor(private contactService: ContactService, private router: Router,
+              private activatedRoute: ActivatedRoute, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-   this.activatedRoute.queryParams.subscribe((params: Params) => {
-     this.newContact = {
-       firstName: '',
-       lastName: params['lastName'],
-       phone: '',
-       email: ''
-     };
-   });
+    let lastName: string = '';
+    this.activatedRoute.queryParams.subscribe((params: Params) => {
+      lastName = params['lastName'];
+    });
+
+    this.contactForm = this.formBuilder.group({
+      'firstName': ['', Validators.required],
+      'lastName': [lastName, Validators.required],
+      'phone': ['', Validators.required],
+      'email': ['', Validators.required]
+    });
   }
 
-  onSubmit() {
-    this.contactService.insertContact(this.newContact);
+  onSubmit(form: NgForm) {
+    this.contactService.insertContact(form.value);
     this.router.navigate(['/contacts'])
       .catch(error => console.log(error));
   }
