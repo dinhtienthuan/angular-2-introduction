b21246d22774ec336515f6a799e87463da2ef289
Thuan T. DINH <dinhtienthuan@gmail.com>
2016-12-26 17:27:51 +0700

    Implemented: Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,44 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <header>
-      <nav>
-        <a [routerLink]="['contacts']">Contacts</a>
-        <a [routerLink]="['new-contacts']">New Contacts</a>
-      </nav>
-    </header>
-    <div class="main">
-      <router-outlet></router-outlet>
-      <app-http-test></app-http-test>
-      <div class="pipes">
-        <h2>Date Pipe</h2>
-        <div>
-          {{date | date:'short'}}
-        </div>
-        <h2>Number Pipe</h2>
-        <div>
-          {{ 4.5 | number:'1.2-2'}}
-        </div>
-        <h2>Currency Pipe</h2>
-        <div>
-          {{ 15.99 | currency:'VND':true:'1.4-4' }}
-        </div>
-        <h2>Stateful Pipe</h2>
-        <div>
-          {{ randomData | async }}
-        </div>
-      </div>
+    <div class="parent">
+    <h1>Parent</h1>
+    <p>Value entered in child component: </p>
+    <input type="text" #parentInput (keyup)="0"><br>
+    <button>Click me</button>
+    <div class="child">
+      <child [parentValue]="parentInput.value"></child>
     </div>
+  </div>
   `
 })
 export class AppComponent {
-  date = new Date();
-  randomData = new Promise((resolve, reject) => {
-    setTimeout(() => resolve('Random data!'), 1000);
-  });
-  public selectedContact = {};
 
-  onSelect(contact) {
-    this.selectedContact = contact;
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,24 @@
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ContactListComponent } from './contact/contact-list.component';
-import { ContactComponent } from './contact/contact.component';
+import { ChildComponent } from './child.component';
 
-import { NewContactComponent } from './contact/new-contact.component';
-import {AppRoutingModule} from "./app.routing";
-import {ContactService} from "./contact/contact.service";
-import { HttpTestComponent } from './http-test/http-test.component';
-import {HttpTestService} from "./http-test/http-test.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactListComponent,
-    ContactComponent,
-    NewContactComponent,
-    HttpTestComponent
+    ChildComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule,
-    AppRoutingModule
+    HttpModule
   ],
-  providers: [ContactService, HttpTestService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
